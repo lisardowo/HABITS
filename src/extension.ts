7ccc5ab3d701f3cb1f4415066e2106f7e417afde
Lisardo Sobrado Morales <lisardowo@gmail.com>
2025-12-24 17:58:32 -0600

    primer prototipo funcional

--- src/extension.ts
+++ src/extension.ts
@@ -2,59 +2,93 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const commandID = `habits.helloWorld`;
+const activationMesagge = `Habits: code session initialized`;
+let statusBar: vscode.StatusBarItem; 
+
+vscode.window.showInformationMessage(activationMesagge);
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-	let timer: ReturnType<typeof setInterval> | undefined;
-    
 	
-    function startTimer() {
+	const HabitsStatus = vscode.commands.registerCommand(`habits.status`, () =>{
+		vscode.window.showInformationMessage(`Habits: OK`);
+	});
+    context.subscriptions.push(HabitsStatus);
+	statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+	statusBar.command = commandID;  
+    statusBar.text = `amongos`;
+
+	startTimer(statusBar);
+	
+	
+
+
+}
+
+
+ function startTimer(statusBar: vscode.StatusBarItem) {
 		
+		let timer: ReturnType<typeof setInterval> | undefined;
 		let seconds = 0;
 		let minutes = 0;
 		let hours = 0;
-     
+		
+		statusBar.text = `Session: ${hours}h ${minutes}m`;
+		statusBar.show();
+
 		if (!timer) {
             timer = setInterval(() => {
                 seconds++;
-                console.log(`has been ${seconds} since the extension was up`);
-                if (seconds === 60) {
+                console.log(`has been ${seconds} since the extension was up`); //TODO remove debug log
+                if (seconds === 60) { //TODO refactorice for more legible code (use negations)
                     seconds = 0;
 					minutes++;
-					console.log(`has passed ${minutes} minutes`);
+					updateStatusBar(hours , minutes);
+					StandRight(minutes);
+					DrinkWater(minutes);
 					if(minutes === 60){
+
+						leaveTheChair();
 						minutes = 0;
 						hours++;
-						console.log(`has passed ${hours} hours`);
+						
 					}
 
                 }
             }, 1000);
         }
-    }
-	startTimer();
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "habits" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('habits.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Habits!');
-	});
-	const prueba1 = vscode.commands.registerCommand('habits.SEXOTILIN', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Asi creas comandos!');
-	});
-
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(prueba1);
+		function updateStatusBar(hours:Number, minutes:Number): void{
+		statusBar.text = `Session: ${hours}h ${minutes}m`;
 }
 
-// This method is called when your extension is deactivated
+function DrinkWater(minutes:Number): void{
+	if(minutes !== 30){
+		return;
+	}
+	else{
+		
+		vscode.window.showInformationMessage(`You better take a sip!`);
+
+	}
+}
+
+function leaveTheChair(): void{
+	vscode.window.showInformationMessage(`Leave the chair and take a little walk`);
+}
+
+function StandRight(minutes:Number): void{
+	if(minutes !== 45){
+		return;
+	}
+	else{
+		vscode.window.showInformationMessage(`Hey!, take care of that back or you'll be sore\n Righten that back and do some stretches`);
+	}
+	
+	}
+
+}
+
 export function deactivate() {}
