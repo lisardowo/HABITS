5e58f8a4289ba16d520c98e7aab7038ebfbfcd11
Lisardo Sobrado Morales <lisardowo@gmail.com>
2025-12-24 14:02:11 -0600

    simple timer

---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let timer: ReturnType<typeof setInterval> | undefined;
+    
+	
+    function startTimer() {
+		
+		let seconds = 0;
+		let minutes = 0;
+		let hours = 0;
+     
+		if (!timer) {
+            timer = setInterval(() => {
+                seconds++;
+                console.log(`has been ${seconds} since the extension was up`);
+                if (seconds === 60) {
+                    seconds = 0;
+					minutes++;
+					console.log(`has passed ${minutes} minutes`);
+					if(minutes === 60){
+						minutes = 0;
+						hours++;
+						console.log(`has passed ${hours} hours`);
+					}
+
+                }
+            }, 1000);
+        }
+    }
+	startTimer();
+	
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "habits" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('habits.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from Habits!');
+	});
+	const prueba1 = vscode.commands.registerCommand('habits.SEXOTILIN', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Asi creas comandos!');
+	});
+
+	context.subscriptions.push(disposable);
+	context.subscriptions.push(prueba1);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
